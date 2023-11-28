163ee5755b2848b30fca19938600db32564f1ae5
Milanikukuma <kukumamilani62@gmail.com>
2023-11-28 08:44:50 +0200

    first commit

---
+++ CountTheNegativeAndPositiveNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercise6
+{
+    internal class CountTheNegativeAndPositiveNumbers
+    {
+        public static int[] CountPosandNegNumbers(int[] numbers)
+        {
+            int negativeNumbers = 0;
+            int positiveNumbers = 0;
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    negativeNumbers++;
+                }
+
+
+                
+
+
+                if (numbers[i] > 0)
+                {
+                    positiveNumbers++;
+                }
+
+
+            }
+            int[] result = new int[2];
+            result[0] = negativeNumbers;
+            result[1] = positiveNumbers;
+
+            return result;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassExercise6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] allNumbers = { -1, 2, 4, 6, 9 };
+
+            int[] result = CountTheNegativeAndPositiveNumbers.CountPosandNegNumbers(allNumbers);
+            string returnString = "Total Positive: " + result[1] + " Total Negative: " + result[0];
+            Console.WriteLine(returnString);
+
+
+
+        }
+    }
+}
